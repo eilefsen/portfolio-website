aabf51de8d24b2b0ae28ee6483716afc201728e1
Emma Eilefsen Glenna <emma@eilefsen.net>
2024-03-18 17:46:50 +0100

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,91 @@
 import { PropsWithChildren } from "react";
 import "./App.css";
-import { Hr } from "./components/util";
+import { CentralHr, LeftHr } from "./components/util";
 
 function App() {
 	return (
 		<>
-			<header className="pb-2">
+			<header className="pb-4">
 				<h1 className="text-7xl">Eilefsen</h1>
-				<Hr />
+				<CentralHr />
 			</header>
-			<main>
-				<div className="mx-auto grid grid-cols-2 gap-x-8 gap-y-4 px-4">
+			<main className="mx-auto min-h-[300px] px-4">
+				<div className="gap-6 sm:columns-2 ">
 					<Paragraph heading="Who am i?">
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-						eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-						ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-						aliquip ex ea commodo consequat.
+						My name is Emma, and I am a young
+						<sub className="text-xs italic">b.2001</sub> software developer
+						living in Oslo.
+						<img
+							className="float-start my-2 me-4 w-24 rounded-xl "
+							src="/profile_pic.jpg"
+							alt="A picture of me!"
+						/>
+						I have always been into IT, and since my early teens, coding. Other
+						than coding, I enjoy making and listening to music of many genres. I
+						also really like photography, both analog and digital.
 					</Paragraph>
 					<Paragraph heading="What do i do?">
-						Duis aute irure dolor in reprehenderit in voluptate velit esse
-						cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-						cupidatat non proident, sunt in culpa qui officia deserunt mollit
-						anim id est laborum.
+						I make things! Particularly software applications. I have also been
+						known to make music out of my bedroom! I mostly make electronic
+						music like breakcore, house, techno. But I have also done some
+						alternative rock previously.
 					</Paragraph>
-					<Paragraph heading="Some cool stuff I've made">
-						<ProjectList />
+					<ProjectList heading="Some cool stuff I've made" />
+				</div>
+				<h2 className="-mb-1 pt-8 text-left text-5xl">More cool stuff</h2>
+				<LeftHr className="mb-2" />
+				<div className="gap-6 sm:columns-2 ">
+					<Paragraph heading="Lorem Ipsum">
+						Lorem ipsum dolor sit amet, officia excepteur ex fugiat
+						reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit
+						ex esse exercitation amet. Nisi anim cupidatat excepteur officia.
+						Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate
+						voluptate dolor minim nulla est proident. Nostrud officia pariatur
+						ut officia. Sit irure elit esse ea nulla sunt ex occaecat
+						reprehenderit commodo officia dolor Lorem duis laboris cupidatat
+						officia voluptate. Culpa proident adipisicing id nulla nisi laboris
+						ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo
+						ex non excepteur duis sunt velit enim. Voluptate laboris sint
+						cupidatat ullamco ut ea consectetur et est culpa et culpa duis.
+					</Paragraph>
+					<Paragraph heading="Lorem Ipsum">
+						Lorem ipsum dolor sit amet, officia excepteur ex fugiat
+						reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit
+						ex esse exercitation amet. Nisi anim cupidatat excepteur officia.
+						Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate
+						voluptate dolor minim nulla est proident. Nostrud officia pariatur
+						ut officia. Sit irure elit esse ea nulla sunt ex occaecat
+						reprehenderit commodo officia dolor Lorem duis laboris cupidatat
+						officia voluptate. Culpa proident adipisicing id nulla nisi laboris
+						ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo
+						ex non excepteur duis sunt velit enim. Voluptate laboris sint
+						cupidatat ullamco ut ea consectetur et est culpa et culpa duis.
+					</Paragraph>
+					<Paragraph heading="Lorem Ipsum">
+						Lorem ipsum dolor sit amet, officia excepteur ex fugiat
+						reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit
+						ex esse exercitation amet. Nisi anim cupidatat excepteur officia.
+						Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate
+						voluptate dolor minim nulla est proident. Nostrud officia pariatur
+						ut officia. Sit irure elit esse ea nulla sunt ex occaecat
+						reprehenderit commodo officia dolor Lorem duis laboris cupidatat
+						officia voluptate. Culpa proident adipisicing id nulla nisi laboris
+						ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo
+						ex non excepteur duis sunt velit enim. Voluptate laboris sint
+						cupidatat ullamco ut ea consectetur et est culpa et culpa duis.
 					</Paragraph>
 				</div>
 			</main>
-			<footer className="pt-6">
-				<Hr />
-				<p className="flex justify-between px-6 pt-2">
+			<footer className="sticky bottom-0 pt-6">
+				<CentralHr />
+				<p className="flex justify-between bg-white px-6 pb-4 pt-2">
 					<span>
 						Email: <a href="mailto:emma@eilefsen.net">emma@eilefsen.net</a>
+					</span>
+					<span>
+						CV: <a href="/cv_english.pdf">English</a>
+						{", "}
+						<a href="/cv_english.pdf">Norwegian</a>
 					</span>
 				</p>
 			</footer>
@@ -41,34 +94,39 @@ function App() {
 }
 
 interface ParagraphProps {
-	heading: string;
+	heading?: string;
 }
 
 function Paragraph(props: PropsWithChildren<ParagraphProps>) {
 	return (
-		<p className="text-justify text-xl">
+		<div className="mb-4 break-inside-avoid text-pretty text-justify text-xl">
 			<h2 className="text-2xl font-bold">{props.heading}</h2>
 			{props.children}
-		</p>
+		</div>
 	);
 }
 
-function ProjectList() {
+interface ProjectListProps extends ParagraphProps {}
+
+function ProjectList(props: ProjectListProps) {
 	return (
-		<ul className="list-inside">
-			<ProjectListItem
-				name="Dotify"
-				description="a music player for the web."
-				link="https://dotify.eilefsen.net"
-				githubLink="https://github.com/eilefsen/dotify"
-			/>
-			<ProjectListItem
-				name="ESP-HTTP-Client"
-				description="an http client for the esp32."
-				link="https://github.com/eilefsen/esp-http-client"
-				githubLink="https://github.com/eilefsen/esp-http-client"
-			/>
-		</ul>
+		<div className="mb-4 break-inside-avoid text-pretty text-justify text-xl">
+			<h3 className="text-2xl font-bold">{props.heading}</h3>
+			<ul className="list-inside ">
+				<ProjectListItem
+					name="Dotify"
+					description="a music player for the web."
+					link="https://dotify.eilefsen.net"
+					githubLink="https://github.com/eilefsen/dotify"
+				/>
+				<ProjectListItem
+					name="ESP-HTTP-Client"
+					description="an http client for the esp32."
+					link="https://github.com/eilefsen/esp-http-client"
+					githubLink="https://github.com/eilefsen/esp-http-client"
+				/>
+			</ul>
+		</div>
 	);
 }
 
--- src/components/util.tsx
+++ src/components/util.tsx
@@ -2,11 +2,34 @@ interface HrProps {
 	className?: string;
 }
 
-export function Hr(props: HrProps) {
+export function CentralHr(props: HrProps) {
 	return (
 		<hr
 			className={
-				"clear-both h-px border-none bg-gradient-to-r from-transparent via-gray-900 to-transparent" +
+				"clear-both h-px border-none bg-gradient-to-r from-transparent via-gray-900 " +
+				" " +
+				props.className
+			}
+		/>
+	);
+}
+
+export function LeftHr(props: HrProps) {
+	return (
+		<hr
+			className={
+				"clear-both h-px border-none bg-gradient-to-r from-gray-900" +
+				" " +
+				props.className
+			}
+		/>
+	);
+}
+export function RightHr(props: HrProps) {
+	return (
+		<hr
+			className={
+				"clear-both h-px border-none bg-gradient-to-l from-gray-900" +
 				" " +
 				props.className
 			}
