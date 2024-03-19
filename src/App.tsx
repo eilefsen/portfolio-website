3bf09fe1faa56c2bd2345d77b248be27ac107129
Emma Eilefsen Glenna <emma@eilefsen.net>
2024-03-19 14:40:24 +0100

    add thoughts section

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { PropsWithChildren } from "react";
+import { PropsWithChildren, ReactElement } from "react";
 import "./App.css";
 import { CentralHr, LeftHr } from "./components/util";
 
@@ -10,19 +10,19 @@ function App() {
 				<CentralHr />
 			</header>
 			<main className="mx-auto min-h-[300px] px-4">
-				<Section id="about">
+				<Section id="about" className="sm:columns-2">
 					<Paragraph heading="Who am i?">
-						My name is Emma, and I am a young
-						<sub className="text-xs italic">b.2001</sub>{" "}
 						<img
-							className="float-start me-4 mt-1 w-32 rounded-lg "
+							className="float-start me-16 mt-2 w-32 rounded-full ring-1 ring-neutral-800 [shape-outside:circle(30%)]"
 							src="/profile_pic.jpg"
 							alt="A picture of me!"
 						/>
-						software developer living in Oslo. I have always been very into IT,
-						and since my early teens, <i>coding</i>. Other than coding, I enjoy
-						making and listening to music of many genres. I also really like
-						photography, both analog and digital.
+						My name is Emma, and I am a young
+						<sub className="text-xs italic">b.2001</sub> software developer
+						living in Oslo. I have always been very into IT, and since my early
+						teens, <i>coding</i>. Other than coding, I enjoy making and
+						listening to music of many genres. I also really like photography,
+						both analog and digital.
 					</Paragraph>
 					<Paragraph heading="What do i do?">
 						I make things! Particularly software applications. I have also been
@@ -31,45 +31,43 @@ function App() {
 						alternative rock previously.
 					</Paragraph>
 				</Section>
-				<Section id="projects" heading="Cool stuff I've made">
+				<Section
+					id="projects"
+					heading="Cool stuff I've made"
+					className="sm:columns-2"
+				>
 					<Paragraph
 						heading="Dotify"
 						url="https://dotify.eilefsen.net"
 						links={[
-							{ text: "github", url: "https://github.com/eilefsen/dotify" },
+							<>
+								<a href="https://dotify.eilefsen.net">live website</a>{" "}
+								<a href="https://github.com/eilefsen/dotify">github</a>
+							</>,
 						]}
 					>
 						Music player for the web. Built with React (MobX + react-router) and
 						Go.{" "}
 						<img
-							className="float-start me-4 mt-1 w-32 rounded-lg"
+							className="float-start me-2 mt-1 w-32 rounded-lg ring-1 ring-neutral-800"
 							src="/dotify.jpg"
 							alt="A picture of Dotify on mobile"
 						/>
 						The songs are both stored as simple audio files in static storage,
 						and metadata along with the local path to each file is stored in the
-						database.
-						<div className="indent-4">
-							You can browse by artists, albums, and all songs. You can play,
-							skip, seek. It is a full fledged music application. The only major
-							feature it lacks is playlists. Personally, playlists are not a
-							feature i use commonly, as i prefer to listen to entire albums
-							instead.
-						</div>
+						database. You can browse by artists, albums, and all songs. You can
+						play, skip, seek. It is a full fledged music application.
 					</Paragraph>
 					<Paragraph
-						heading="ESP-HTTP-Client"
+						heading="Bus departures board"
 						links={[
-							{
-								text: "github",
-								url: "https://github.com/eilefsen/esp-http-client",
-							},
+							<a href="https://github.com/eilefsen/esp-http-client">github</a>,
 						]}
 					>
 						This is a fairly basic HTTP client for the ESP32. The
 						microcontroller drives a sh1106 OLED display.
 						<img
-							className="float-start me-4 mt-1 h-48 rounded-lg"
+							className="float-start me-2 mt-1 h-40 rounded-lg ring-1 ring-neutral-800"
 							src="/esp_http_client.gif"
 							alt="A picture of Dotify on mobile"
 						/>{" "}
@@ -83,6 +81,32 @@ function App() {
 					</Paragraph>
 				</Section>
 			</main>
+			<Section id="thoughts" heading="Thoughts" className="sm:columns-3">
+				<Paragraph heading="Shrimp fried rice">
+					A shrimp did not fry this rice. I cannot believe it.
+				</Paragraph>
+				<Paragraph heading="Shrimp fried rice">
+					A shrimp did not fry this rice. I cannot believe it.
+				</Paragraph>
+				<Paragraph heading="Shrimp fried rice">
+					A shrimp did not fry this rice. I cannot believe it.
+				</Paragraph>
+				<Paragraph heading="Shrimp fried rice">
+					A shrimp did not fry this rice. I cannot believe it.
+				</Paragraph>
+				<Paragraph heading="Shrimp fried rice">
+					A shrimp did not fry this rice. I cannot believe it.
+				</Paragraph>
+				<Paragraph heading="Shrimp fried rice">
+					A shrimp did not fry this rice. I cannot believe it.
+				</Paragraph>
+				<Paragraph heading="Shrimp fried rice">
+					A shrimp did not fry this rice. I cannot believe it.
+				</Paragraph>
+				<Paragraph heading="Shrimp fried rice">
+					A shrimp did not fry this rice. I cannot believe it.
+				</Paragraph>
+			</Section>
 			<footer className="sticky bottom-0 pt-6">
 				<CentralHr />
 				<p className="flex justify-between bg-white px-6 pb-4 pt-2">
@@ -103,6 +127,7 @@ function App() {
 interface SectionProps {
 	id?: string;
 	heading?: string;
+	className?: string;
 }
 
 function Section(props: PropsWithChildren<SectionProps>) {
@@ -122,7 +147,7 @@ function Section(props: PropsWithChildren<SectionProps>) {
 					<LeftHr className="mb-2" />
 				</>
 			)}
-			<div className="gap-6 sm:columns-2 ">{props.children}</div>
+			<div className={"gap-6" + " " + props.className}>{props.children}</div>
 		</section>
 	);
 }
@@ -130,18 +155,10 @@ function Section(props: PropsWithChildren<SectionProps>) {
 interface ParagraphProps {
 	heading?: string;
 	url?: string;
-	links?: [{ url: string; text: string }];
+	links?: [ReactElement<HTMLAnchorElement>];
 }
 
 function Paragraph(props: PropsWithChildren<ParagraphProps>) {
-	const linkElements = [];
-	if (props.links) {
-		for (const link of props.links) {
-			const el = <a href={link.url}>{link.text}</a>;
-			linkElements.push(el);
-		}
-	}
-
 	return (
 		<div className="break-inside-avoid-column pb-4 ">
 			<div className="text-left">
@@ -150,11 +167,11 @@ function Paragraph(props: PropsWithChildren<ParagraphProps>) {
 						{props.heading}
 					</a>
 				</h3>{" "}
-				{linkElements}
+				{props.links}
 			</div>
-			<div className="hyphens-auto text-pretty text-justify text-xl">
+			<p className="hyphens-auto text-pretty text-justify text-xl">
 				{props.children}
-			</div>
+			</p>
 		</div>
 	);
 }
