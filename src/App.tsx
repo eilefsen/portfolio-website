c28289d1112db4e1a9b4bbfb6d96cd0c4c1f31a1
Emma Eilefsen Glenna <emma@eilefsen.net>
2024-03-19 11:43:17 +0100

    add projects

--- src/App.tsx
+++ src/App.tsx
@@ -6,23 +6,23 @@ function App() {
 	return (
 		<>
 			<header className="pb-4">
-				<h1 className="text-7xl">Eilefsen</h1>
+				<h1 className="-mb-1 text-7xl">Eilefsen</h1>
 				<CentralHr />
 			</header>
 			<main className="mx-auto min-h-[300px] px-4">
-				<div className="gap-6 sm:columns-2 ">
+				<Section id="about">
 					<Paragraph heading="Who am i?">
 						My name is Emma, and I am a young
-						<sub className="text-xs italic">b.2001</sub> software developer
-						living in Oslo.
+						<sub className="text-xs italic">b.2001</sub>{" "}
 						<img
-							className="float-start my-2 me-4 w-24 rounded-xl "
+							className="float-start me-4 mt-1 w-32 rounded-lg "
 							src="/profile_pic.jpg"
 							alt="A picture of me!"
 						/>
-						I have always been into IT, and since my early teens, coding. Other
-						than coding, I enjoy making and listening to music of many genres. I
-						also really like photography, both analog and digital.
+						software developer living in Oslo. I have always been very into IT,
+						and since my early teens, <i>coding</i>. Other than coding, I enjoy
+						making and listening to music of many genres. I also really like
+						photography, both analog and digital.
 					</Paragraph>
 					<Paragraph heading="What do i do?">
 						I make things! Particularly software applications. I have also been
@@ -30,51 +30,58 @@ function App() {
 						music like breakcore, house, techno. But I have also done some
 						alternative rock previously.
 					</Paragraph>
-					<ProjectList heading="Some cool stuff I've made" />
-				</div>
-				<h2 className="-mb-1 pt-8 text-left text-5xl">More cool stuff</h2>
-				<LeftHr className="mb-2" />
-				<div className="gap-6 sm:columns-2 ">
-					<Paragraph heading="Lorem Ipsum">
-						Lorem ipsum dolor sit amet, officia excepteur ex fugiat
-						reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit
-						ex esse exercitation amet. Nisi anim cupidatat excepteur officia.
-						Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate
-						voluptate dolor minim nulla est proident. Nostrud officia pariatur
-						ut officia. Sit irure elit esse ea nulla sunt ex occaecat
-						reprehenderit commodo officia dolor Lorem duis laboris cupidatat
-						officia voluptate. Culpa proident adipisicing id nulla nisi laboris
-						ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo
-						ex non excepteur duis sunt velit enim. Voluptate laboris sint
-						cupidatat ullamco ut ea consectetur et est culpa et culpa duis.
+				</Section>
+				<Section id="projects" heading="Cool stuff I've made">
+					<Paragraph
+						heading="Dotify"
+						url="https://dotify.eilefsen.net"
+						links={[
+							{ text: "github", url: "https://github.com/eilefsen/dotify" },
+						]}
+					>
+						Music player for the web. Built with React (MobX + react-router) and
+						Go.{" "}
+						<img
+							className="float-start me-4 mt-1 w-32 rounded-lg"
+							src="/dotify.jpg"
+							alt="A picture of Dotify on mobile"
+						/>
+						The songs are both stored as simple audio files in static storage,
+						and metadata along with the local path to each file is stored in the
+						database.
+						<div className="indent-4">
+							You can browse by artists, albums, and all songs. You can play,
+							skip, seek. It is a full fledged music application. The only major
+							feature it lacks is playlists. Personally, playlists are not a
+							feature i use commonly, as i prefer to listen to entire albums
+							instead.
+						</div>
 					</Paragraph>
-					<Paragraph heading="Lorem Ipsum">
-						Lorem ipsum dolor sit amet, officia excepteur ex fugiat
-						reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit
-						ex esse exercitation amet. Nisi anim cupidatat excepteur officia.
-						Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate
-						voluptate dolor minim nulla est proident. Nostrud officia pariatur
-						ut officia. Sit irure elit esse ea nulla sunt ex occaecat
-						reprehenderit commodo officia dolor Lorem duis laboris cupidatat
-						officia voluptate. Culpa proident adipisicing id nulla nisi laboris
-						ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo
-						ex non excepteur duis sunt velit enim. Voluptate laboris sint
-						cupidatat ullamco ut ea consectetur et est culpa et culpa duis.
+					<Paragraph
+						heading="ESP-HTTP-Client"
+						links={[
+							{
+								text: "github",
+								url: "https://github.com/eilefsen/esp-http-client",
+							},
+						]}
+					>
+						This is a fairly basic HTTP client for the ESP32. The
+						microcontroller drives a sh1106 OLED display.
+						<img
+							className="float-start me-4 mt-1 h-48 rounded-lg"
+							src="/esp_http_client.gif"
+							alt="A picture of Dotify on mobile"
+						/>{" "}
+						The purpose is to monitor local bus departures so i can check it
+						before leaving the house, potentially saving me a few minutes out in
+						the cold. The API i'm targeting is specific to Norway, although it
+						is based on a larger project likely used elsewhere. The crates for
+						esp are not super well documented (imo), so this took a bit of
+						guesswork to implement. PS: yes, the display is in fact just taped
+						to the board (lol).
 					</Paragraph>
-					<Paragraph heading="Lorem Ipsum">
-						Lorem ipsum dolor sit amet, officia excepteur ex fugiat
-						reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit
-						ex esse exercitation amet. Nisi anim cupidatat excepteur officia.
-						Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate
-						voluptate dolor minim nulla est proident. Nostrud officia pariatur
-						ut officia. Sit irure elit esse ea nulla sunt ex occaecat
-						reprehenderit commodo officia dolor Lorem duis laboris cupidatat
-						officia voluptate. Culpa proident adipisicing id nulla nisi laboris
-						ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo
-						ex non excepteur duis sunt velit enim. Voluptate laboris sint
-						cupidatat ullamco ut ea consectetur et est culpa et culpa duis.
-					</Paragraph>
-				</div>
+				</Section>
 			</main>
 			<footer className="sticky bottom-0 pt-6">
 				<CentralHr />
@@ -93,58 +100,62 @@ function App() {
 	);
 }
 
-interface ParagraphProps {
+interface SectionProps {
+	id?: string;
 	heading?: string;
 }
 
+function Section(props: PropsWithChildren<SectionProps>) {
+	let url;
+	if (props.id) {
+		url = "/#" + props.id;
+	}
+	return (
+		<section id={props.id}>
+			{props.heading && (
+				<>
+					<h2 className="-mb-0.5 pt-8 text-left text-4xl">
+						<a href={url} className="no-underline">
+							{props.heading}
+						</a>
+					</h2>
+					<LeftHr className="mb-2" />
+				</>
+			)}
+			<div className="gap-6 sm:columns-2 ">{props.children}</div>
+		</section>
+	);
+}
+
+interface ParagraphProps {
+	heading?: string;
+	url?: string;
+	links?: [{ url: string; text: string }];
+}
+
 function Paragraph(props: PropsWithChildren<ParagraphProps>) {
+	const linkElements = [];
+	if (props.links) {
+		for (const link of props.links) {
+			const el = <a href={link.url}>{link.text}</a>;
+			linkElements.push(el);
+		}
+	}
+
 	return (
-		<div className="mb-4 break-inside-avoid text-pretty text-justify text-xl">
-			<h2 className="text-2xl font-bold">{props.heading}</h2>
-			{props.children}
+		<div className="break-inside-avoid-column pb-4 ">
+			<div className="text-left">
+				<h3 className="inline text-2xl font-bold">
+					<a className="no-underline" href={props.url}>
+						{props.heading}
+					</a>
+				</h3>{" "}
+				{linkElements}
+			</div>
+			<div className="hyphens-auto text-pretty text-justify text-xl">
+				{props.children}
+			</div>
 		</div>
-	);
-}
-
-interface ProjectListProps extends ParagraphProps {}
-
-function ProjectList(props: ProjectListProps) {
-	return (
-		<div className="mb-4 break-inside-avoid text-pretty text-justify text-xl">
-			<h3 className="text-2xl font-bold">{props.heading}</h3>
-			<ul className="list-inside ">
-				<ProjectListItem
-					name="Dotify"
-					description="a music player for the web."
-					link="https://dotify.eilefsen.net"
-					githubLink="https://github.com/eilefsen/dotify"
-				/>
-				<ProjectListItem
-					name="ESP-HTTP-Client"
-					description="an http client for the esp32."
-					link="https://github.com/eilefsen/esp-http-client"
-					githubLink="https://github.com/eilefsen/esp-http-client"
-				/>
-			</ul>
-		</div>
-	);
-}
-
-interface ProjectListItemProps {
-	link: string;
-	name: string;
-	description: string;
-	githubLink: string;
-}
-
-function ProjectListItem(props: ProjectListItemProps) {
-	return (
-		<li>
-			<a href={props.link}>{props.name}</a>, {props.description + " "}
-			<small className="text-sm">
-				<a href={props.githubLink}>github</a>
-			</small>
-		</li>
 	);
 }
 
