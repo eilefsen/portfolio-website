4efc13fce0d7eb94f1d537fc251a0bacc6a2a80b
Emma Eilefsen Glenna <emma@eilefsen.net>
2024-03-21 00:15:02 +0100

    add thought form

--- frontend/src/components/thoughtForm.tsx
+++ frontend/src/components/thoughtForm.tsx
@@ -1,6 +1,25 @@
+"use client";
 import { LeftHr } from "./util";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+const formSchema = z.object({
+	heading: z.string().min(2).max(50),
+	body: z.string().min(2).max(250),
+	dateTimeCreated: z.string().min(19).max(19),
+});
 
 export function ThoughtForm() {
+	const form = useForm<z.infer<typeof formSchema>>({
+		resolver: zodResolver(formSchema),
+		defaultValues: {
+			heading: "",
+			body: "",
+			dateTimeCreated: new Date().toISOString().slice(0, 19).replace("T", " "),
+		},
+	});
+
 	function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
 		e.preventDefault();
 
@@ -14,7 +33,7 @@ export function ThoughtForm() {
 		const data = {
 			heading: formElements.heading.value,
 			body: formElements.body.value,
-			dateTimeCreated: new Date().toString(),
+			dateTimeCreated: new Date().toISOString().slice(0, 19).replace("T", " "),
 		};
 
 		fetch(finalFormEndpoint, {
@@ -33,14 +52,20 @@ export function ThoughtForm() {
 		});
 	}
 
+	function onSubmit(values: z.infer<typeof formSchema>) {
+		// Do something with the form values.
+		// ✅ This will be type-safe and validated.
+		console.log(values);
+	}
+
 	return (
-		<div className="text-justify">
+		<div className="w-full max-w-96 text-justify">
 			<h2 className="-mb-1 text-2xl">Submit new Thought</h2>
-			<LeftHr className="from-neutral-500 to-30%" />
+			<LeftHr className="mb-1.5 from-neutral-500" />
 			<form
-				className="grid grid-cols-[15%_85%] grid-rows-2 gap-2"
+				className="grid grid-cols-[25%_75%] grid-rows-2 gap-2"
 				method="POST"
-				action="/api/thought/create"
+				action="/api/thoughts/create"
 				onSubmit={handleSubmit}
 			>
 				<label htmlFor="heading" className="text-lg font-bold">
@@ -49,7 +74,7 @@ export function ThoughtForm() {
 				<input
 					name="heading"
 					type="text"
-					className="rounded-sm border border-neutral-500"
+					className="rounded-sm border border-neutral-400"
 				/>
 				<label htmlFor="body" className="text-lg font-bold">
 					Body
@@ -57,9 +82,9 @@ export function ThoughtForm() {
 				<input
 					name="body"
 					type="text"
-					className="rounded-sm border border-neutral-500"
+					className="rounded-sm border border-neutral-400"
 				/>
-				<input type="submit" />
+				<input type="submit" className="sm:hidden" />
 			</form>
 		</div>
 	);
