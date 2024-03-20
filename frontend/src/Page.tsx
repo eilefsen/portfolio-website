1efb8db4857102dbc34966dd1adc3474f56f9726
Emma Eilefsen Glenna <emma@eilefsen.net>
2024-03-21 00:14:46 +0100

    design change

--- frontend/src/Page.tsx
+++ frontend/src/Page.tsx
@@ -45,6 +45,30 @@ export default function Page() {
 					className="sm:columns-2 "
 				>
 					<Paragraph
+						heading="Bus departures board"
+						links={[
+							{
+								url: "https://github.com/eilefsen/esp-http-client",
+								text: "github",
+							},
+						]}
+					>
+						This is a fairly basic HTTP client for the ESP32. The
+						microcontroller drives a sh1106 OLED display.
+						<img
+							className="float-start me-3 mt-1 h-40 rounded-sm shadow-md shadow-neutral-400"
+							src="/esp_http_client.gif"
+							alt="A picture of Dotify on mobile"
+						/>{" "}
+						The purpose is to monitor local bus departures so i can check it
+						before leaving the house, potentially saving me a few minutes out in
+						the cold. The API i'm targeting is specific to Norway, although it
+						is based on a larger project likely used elsewhere. The crates for
+						esp are not super well documented (imo), so this took a bit of
+						guesswork to implement. PS: yes, the display is in fact just taped
+						to the board (lol).
+					</Paragraph>
+					<Paragraph
 						heading="Dotify"
 						url="https://dotify.eilefsen.net"
 						links={[
@@ -70,36 +94,12 @@ export default function Page() {
 						database. You can browse by artists, albums, and all songs. You can
 						play, skip, seek. It is a full fledged music application.
 					</Paragraph>
-					<Paragraph
-						heading="Bus departures board"
-						links={[
-							{
-								url: "https://github.com/eilefsen/esp-http-client",
-								text: "github",
-							},
-						]}
-					>
-						This is a fairly basic HTTP client for the ESP32. The
-						microcontroller drives a sh1106 OLED display.
-						<img
-							className="float-start me-3 mt-1 h-40 rounded-sm shadow-md shadow-neutral-400"
-							src="/esp_http_client.gif"
-							alt="A picture of Dotify on mobile"
-						/>{" "}
-						The purpose is to monitor local bus departures so i can check it
-						before leaving the house, potentially saving me a few minutes out in
-						the cold. The API i'm targeting is specific to Norway, although it
-						is based on a larger project likely used elsewhere. The crates for
-						esp are not super well documented (imo), so this took a bit of
-						guesswork to implement. PS: yes, the display is in fact just taped
-						to the board (lol).
-					</Paragraph>
 				</Section>
 			</main>
 			<Section
 				id="thoughts"
 				heading="Some of my Thoughts"
-				className="sm:columns-3"
+				className="sm:columns-4"
 			>
 				{thoughts}
 			</Section>
