fee055f0a03e18b3c3baac102ef87087a64bebb9
Emma Eilefsen Glenna <emma@eilefsen.net>
2024-03-21 15:56:38 +0100

    edit sql script

--- backend/models/populate-tables.sql
+++ backend/models/populate-tables.sql
@@ -2,13 +2,8 @@ INSERT INTO
   thought (heading, body, datetime_created)
 VALUES
   (
-    'Shrimp',
-    "it's as shrimple as that",
+    'New website!',
+    "Welcome to my new website!",
     '2024-03-19 15:43:00'
-  ),
-  (
-    'Shrimp fried rice',
-    "No fucking way dude",
-    '2024-03-19 15:44:00'
   )
 
