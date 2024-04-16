95a15636feeb28dc04bce57519463189dabf316a
Emma Eilefsen Glenna <emma@eilefsen.net>
2024-04-16 20:44:33 +0200

    pass through props

--- frontend/src/components/ui/inputfile.tsx
+++ frontend/src/components/ui/inputfile.tsx
@@ -1,11 +1,14 @@
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 
-export function InputFile() {
+export interface InputFileProps
+	extends React.InputHTMLAttributes<HTMLInputElement> {}
+
+export function InputFile(props: InputFileProps) {
 	return (
 		<div className="grid w-full max-w-sm items-center gap-1.5">
 			<Label htmlFor="picture">Picture</Label>
-			<Input id="picture" type="file" />
+			<Input id="picture" type="file" {...props} />
 		</div>
 	);
 }
