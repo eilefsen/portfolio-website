fa9469d92fd6fc290b0cd38365980b51d0255ad5
Emma Eilefsen Glenna <emma@eilefsen.net>
2024-04-16 16:55:03 +0200

    add GalleryCard

--- frontend/src/components/gallery.tsx
+++ frontend/src/components/gallery.tsx
@@ -1,4 +1,39 @@
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardHeader,
+	CardTitle,
+} from "./ui/card";
+
 interface GalleryProps {}
 export function Gallery(props: GalleryProps) {
+	return (
+		<div className="">
+			<GalleryCard
+				title="En katt i natten"
+				locationName="Grunerløkka"
+				imgSrc="/img/katt_i_natten.jpg"
+			/>
+		</div>
+	);
 }
+
+interface GalleryCardProps {
+	title?: string;
+	locationName?: string;
+	imgSrc: string;
+}
+function GalleryCard(props: GalleryCardProps) {
+	return (
+		<Card>
+			<CardHeader>
+				<CardTitle>{props.title}</CardTitle>
+				<CardDescription>{props.locationName}</CardDescription>
+			</CardHeader>
+			<CardContent>
+				<img src={props.imgSrc} alt={props.title} />
+			</CardContent>
+		</Card>
+	);
 }
