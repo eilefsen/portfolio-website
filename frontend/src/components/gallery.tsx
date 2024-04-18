0a68525a48496c7a605707beb2653083476687b4
Emma Eilefsen Glenna <emma@eilefsen.net>
2024-04-18 21:43:27 +0200

    uhhh

--- frontend/src/components/gallery.tsx
+++ frontend/src/components/gallery.tsx
@@ -1,32 +1,40 @@
-import { useForm } from "react-hook-form";
 import { Form } from "@/components/ui/form";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import axios, { AxiosResponse } from "axios";
+import { EmblaCarouselType, EmblaEventType } from "embla-carousel";
+import AutoHeight from "embla-carousel-auto-height";
+import Autoplay from "embla-carousel-autoplay";
+import React, { useCallback, useEffect, useRef } from "react";
+import { useForm } from "react-hook-form";
+import { Section } from "./section";
 import { Button } from "./ui/button";
 import {
 	Carousel,
+	CarouselApi,
 	CarouselContent,
 	CarouselItem,
 	CarouselNext,
 	CarouselPrevious,
 } from "./ui/carousel";
 import {
+	FormControl,
+	FormDescription,
 	FormField,
 	FormItem,
 	FormLabel,
-	FormControl,
-	FormDescription,
 	FormMessage,
 } from "./ui/form";
 import { Input } from "./ui/input";
+import { InputFile } from "./ui/inputfile";
 import { Textarea } from "./ui/textarea";
 import { CentralHr } from "./util";
-import { useQueryClient, useMutation, useQuery } from "@tanstack/react-query";
-import Autoplay from "embla-carousel-autoplay";
-import ClassNames from "embla-carousel-class-names";
-import axios, { AxiosResponse } from "axios";
-import { Section } from "./section";
-import { InputFile } from "./ui/inputfile";
+
+function numberWithinRange(number: number, min: number, max: number): number {
+	return Math.min(Math.max(number, min), max);
+}
 
 export function Gallery() {
+	const [api, setApi] = React.useState<CarouselApi>();
 	const result = useQuery({
 		queryKey: ["pictures"],
 		queryFn: async (): Promise<GalleryPicture[]> => {
@@ -35,16 +43,6 @@ export function Gallery() {
 		},
 		initialData: [],
 	});
-
-	if (result.isFetching) {
-		return null;
-	}
-	if (!result.isSuccess) {
-		return null;
-	}
-	if (!result.data) {
-		return null;
-	}
 
 	const items: JSX.Element[] = [];
 
@@ -60,9 +58,78 @@ export function Gallery() {
 		items.push(item);
 	}
 
+	const TWEEN_FACTOR_BASE = 0.84;
+	const tweenFactor = useRef(0);
+	const setTweenFactor = useCallback((emblaApi: EmblaCarouselType) => {
+		tweenFactor.current = TWEEN_FACTOR_BASE * emblaApi.scrollSnapList().length;
+	}, []);
+
+	const tweenOpacity = useCallback(
+		(emblaApi: EmblaCarouselType, eventName?: EmblaEventType) => {
+			const engine = emblaApi.internalEngine();
+			const scrollProgress = emblaApi.scrollProgress();
+			const slidesInView = emblaApi.slidesInView();
+			const isScrollEvent = eventName === "scroll";
+
+			emblaApi.scrollSnapList().forEach((scrollSnap, snapIndex) => {
+				let diffToTarget = scrollSnap - scrollProgress;
+				const slidesInSnap = engine.slideRegistry[snapIndex];
+
+				slidesInSnap.forEach((slideIndex) => {
+					if (isScrollEvent && !slidesInView.includes(slideIndex)) return;
+
+					if (engine.options.loop) {
+						engine.slideLooper.loopPoints.forEach((loopItem) => {
+							const target = loopItem.target();
+
+							if (slideIndex === loopItem.index && target !== 0) {
+								const sign = Math.sign(target);
+
+								if (sign === -1) {
+									diffToTarget = scrollSnap - (1 + scrollProgress);
+								}
+								if (sign === 1) {
+									diffToTarget = scrollSnap + (1 - scrollProgress);
+								}
+							}
+						});
+					}
+
+					const tweenValue = 1 - Math.abs(diffToTarget * tweenFactor.current);
+					const opacity = numberWithinRange(tweenValue, 0, 1).toString();
+					emblaApi.slideNodes()[slideIndex].style.opacity = opacity;
+				});
+			});
+		},
+		[],
+	);
+
+	useEffect(() => {
+		if (!api) return;
+
+		setTweenFactor(api);
+		tweenOpacity(api);
+		api
+			.on("reInit", setTweenFactor)
+			.on("reInit", tweenOpacity)
+			.on("scroll", tweenOpacity);
+	}, [api, tweenOpacity]);
+
+	// return null if result is not a success or is empty
+	if (result.isFetching) {
+		return null;
+	}
+	if (!result.isSuccess) {
+		return null;
+	}
+	if (!result.data) {
+		return null;
+	}
+
 	return (
 		<Section id="gallery" heading="Gallery">
 			<Carousel
+				setApi={setApi}
 				opts={{
 					align: "center",
 					loop: true,
@@ -71,12 +138,11 @@ export function Gallery() {
 					Autoplay({
 						delay: 5000,
 					}),
-					ClassNames({ snapped: "opacity-100" }),
 				]}
 			>
-				<CarouselContent>{items}</CarouselContent>
-				<CarouselPrevious />
-				<CarouselNext />
+				<CarouselContent className="flex items-start">{items}</CarouselContent>
+				<CarouselPrevious className="hidden sm:flex" />
+				<CarouselNext className="hidden sm:flex" />
 			</Carousel>
 		</Section>
 	);
@@ -101,16 +167,20 @@ interface GalleryCarouselItemProps {
 }
 function GalleryCarouselItem(props: GalleryCarouselItemProps) {
 	return (
-		<CarouselItem className="basis-11/12 opacity-50 transition-opacity duration-700">
-			<div>
-				<h3 className="smallcaps text-3xl font-bold leading-none">
+		<CarouselItem className="basis-auto">
+			<div className="mx-auto hyphens-auto break-words">
+				<h3 className="smallcaps text-xl font-bold leading-none">
 					«{props.title}»
 				</h3>
 				<h4 className="pb-2 text-lg font-light leading-none">
-					{props.locationName}
+					{props.locationName + " "}
 				</h4>
-				<img src={props.imgSrc} className="rounded-lg" alt={props.title} />
 			</div>
+			<img
+				src={props.imgSrc}
+				alt={props.title}
+				className="mx-auto h-full max-h-[210px] rounded-lg object-contain sm:max-h-[400px]"
+			/>
 		</CarouselItem>
 	);
 }
